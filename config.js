c4224c82a53fd709118a7d797c85632257ffac6f
javidei <javyspot@gmail.com>
2026-08-05 14:06:40 +0200

    Añade configuración pública

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuración pública de Book Affinity.
+// La URL y la clave publicable de Supabase pueden estar en el navegador.
+// Nunca añadas aquí la clave service_role ni contraseñas.
+window.BOOK_AFFINITY_CONFIG = Object.freeze({
+  supabaseUrl: '',
+  supabasePublishableKey: '',
+  googleBooksApiKey: '',
+  webVersion: '0.1.0',
+  webReleaseDate: '05/08/2026'
+});
