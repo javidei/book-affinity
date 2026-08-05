dd30a473aa5a86f7576e31e16cc593b75d89cfee
javidei <javyspot@gmail.com>
2026-08-06 00:21:15 +0200

    Añade catálogo de avatares de usuario

---
+++ avatar-options.js
@@ -0,0 +1,8 @@
+// Avatares disponibles para las cuentas de Book Affinity.
+window.BOOK_AFFINITY_AVATARS = Object.freeze([
+  Object.freeze({ id: 'literary-crest', name: 'Emblema literario', src: 'assets/avatars/literary-crest.svg' }),
+  Object.freeze({ id: 'minimal-reader', name: 'Lector minimalista', src: 'assets/avatars/minimal-reader.svg' }),
+  Object.freeze({ id: 'moon-book', name: 'Libro bajo la luna', src: 'assets/avatars/moon-book.svg' }),
+  Object.freeze({ id: 'book-shield', name: 'Escudo de libros', src: 'assets/avatars/book-shield.svg' }),
+  Object.freeze({ id: 'retro-reader', name: 'Lector retro', src: 'assets/avatars/retro-reader.svg' })
+]);
